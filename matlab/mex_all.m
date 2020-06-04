7fb36b9d507c96a7ed3c79d05f7d158454c96552
Jiarui Li <jiarui_li@outlook.com>
2020-06-04 12:44:53 +0800

    update

--- matlab/mex_all.m
+++ matlab/mex_all.m
@@ -1,20 +1,9 @@
-disp('Mexing "compute_correspondences"...')
-mex -I/usr/local/include/ mex/compute_correspondences.cpp -lmeshmonk
-disp('Mexing "compute_inlier_weights"...')
-mex -I/usr/local/include/ mex/compute_inlier_weights.cpp -lmeshmonk
-disp('Mexing "compute_nonrigid_transformation"...')
-mex -I/usr/local/include/ mex/compute_nonrigid_transformation.cpp -lmeshmonk
-disp('Mexing "compute_rigid_transformation"...')
-mex -I/usr/local/include/ mex/compute_rigid_transformation.cpp -lmeshmonk
-disp('Mexing "downsample_mesh"...')
-mex -I/usr/local/include/ mex/downsample_mesh.cpp -lmeshmonk
-disp('Mexing "nonrigid_registration"...')
-mex -I/usr/local/include/ mex/nonrigid_registration.cpp -lmeshmonk
-disp('Mexing "pyramid_registration"...')
-mex -I/usr/local/include/ mex/pyramid_registration.cpp -lmeshmonk
-disp('Mexing "rigid_registration"...')
-mex -I/usr/local/include/ mex/rigid_registration.cpp -lmeshmonk
-disp('Mexing "scaleshift_mesh"...')
-mex -I/usr/local/include/ mex/scaleshift_mesh.cpp -lmeshmonk
-disp('Mexing "compute_normals"...')
-mex -I/usr/local/include/ mex/compute_normals.cpp -lmeshmonk
+function mex_all(prefix_path)
+if nargin<1
+    prefix_path='/usr/local';
+end
+include_path = ['-I',prefix_path,'/include'];
+filelist = dir('mex/*.cpp');
+for i=1:length(filelist)
+    mex(include_path,['mex/',filelist(i).name],'-lmeshmonk');
+end
--- matlab/mex_windows_all.m
+++ matlab/mex_windows_all.m
@@ -6,32 +6,7 @@ c_eigen = ['-I', '%USERPROFILE%\Documents\GitHub\eigen-eigen-3.3.4'];
 c_math = ['-D', '_USE_MATH_DEFINES'];
 c_lib = ['-l', 'meshmonk'];
 
-disp('Mexing "compute_correspondences"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/compute_correspondences.cpp')
-
-disp('Mexing "compute_inlier_weights"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/compute_inlier_weights.cpp')
-
-disp('Mexing "compute_nonrigid_transformation"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/compute_nonrigid_transformation.cpp')
-
-disp('Mexing "compute_rigid_transformation"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/compute_rigid_transformation.cpp')
-
-disp('Mexing "downsample_mesh"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/downsample_mesh.cpp')
-
-disp('Mexing "nonrigid_registration"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/nonrigid_registration.cpp')
-
-disp('Mexing "pyramid_registration"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/pyramid_registration.cpp')
-
-disp('Mexing "rigid_registration"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/rigid_registration.cpp')
-
-disp('Mexing "scaleshift_mesh"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/scaleshift_mesh.cpp')
-
-disp('Mexing "compute_normals"...')
-mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib, 'matlab/mex/compute_normals.cpp')
+filelist = dir('mex/*.cpp');
+for i=1:length(filelist)
+    mex(c_om, c_mesh, c_nano, c_eigen, c_math, c_lib,['mex/',filelist(i).name]);
+end
